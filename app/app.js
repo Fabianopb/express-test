4c74d1c1698543da9203228d681cc2b300bed627
Fabiano Brito <fabianopbrito@gmail.com>
2017-03-13 22:12:04 +0200

    Creates html response and redirects a route.

--- app/app.js
+++ app/app.js
@@ -5,6 +5,20 @@ app.get('/', function(request, response) {
   response.send("Hello World");
 });
 
+app.get('/json_endpoint', function(request, response) {
+  var fruits = ['apples', 'bananas', 'plums'];
+  response.json(fruits);
+});
+
+app.get('/html_endpoint', function(request, response) {
+  var template = '<ul><li>item 1</li><li>item 2</li><li>item 2</li></ul>';
+  response.send(template);
+});
+
+app.get('/redirect_me', function(request, response) {
+  response.redirect(301, '/');
+});
+
 app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
