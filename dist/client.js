befa5a4e8246d884a0071f3b93778143c748b752
Fabiano Brito <fabianopbrito@gmail.com>
2017-03-16 20:48:35 +0200

    Creates form for adding fruits and creates a list of fruits.

--- dist/client.js
+++ dist/client.js
@@ -1,7 +1,10 @@
 $(function() {
 
   $.get('/json_endpoint', function(response) {
-    $('.test-style').html(response.join(', '));
+    var container = $('.test-style').find('ul');
+    for (var i = 0; i < response.length; i++) {
+      container.append('<li><a class="remove" href="#">(x)</a> ' + response[i] + '</li>')
+    }
   });
 
 });
